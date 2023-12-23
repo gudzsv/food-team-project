c9ed4654785b8a7c627607a714d468b0aa94cdb0
Serhii Hudzenko <gudzsvmail@gmail.com>
2023-12-23 11:09:35 +0200

    add

--- js/script.js
+++ js/script.js
@@ -1,33 +1,15 @@
-/* Serhii Hudzenko */
-/* #### Modal #### */
-<<<<<<< Updated upstream
-/* # 10.12.2023  # */
-
-// Crete an object of HTML elements
-=======
-/* # 12.12.2023  # */
-
-// Create an Object
->>>>>>> Stashed changes
 const refs = {
-  openModalBtn: document.querySelector("[data-open-modal]"),
-  closeModalBtn: document.querySelector("[data-close-modal]"),
-  backdrop: document.querySelector("[data-backdrop]"),
+  openModalBtn: document.querySelector('[data-open-modal]'),
+  closeModalBtn: document.querySelector('[data-close-modal]'),
+  backdrop: document.querySelector('[data-backdrop]'),
 };
 
-<<<<<<< Updated upstream
-// Add Events Listener to the HTML elements
-refs.openModalBtn.addEventListener("click", toogleModal);
-refs.closeModalBtn.addEventListener("click", toogleModal);
+refs.openModalBtn.addEventListener('click', toogleModal);
+refs.closeModalBtn.addEventListener('click', toogleModal);
 
-// Create function to change/toogle "is-open" class on the backdrop element to open/close modal window
-=======
-// Add Listener to HTML elements(Tags)
-refs.openModalBtn.addEventListener("click", toogleModal);
-refs.closeModalBtn.addEventListener("click", toogleModal);
+refs.openModalBtn.addEventListener('click', toogleModal);
+refs.closeModalBtn.addEventListener('click', toogleModal);
 
-// Function to change/toogle class on backdrop
->>>>>>> Stashed changes
 function toogleModal() {
-  refs.backdrop.classList.toggle("is-open");
+  refs.backdrop.classList.toggle('is-open');
 }
