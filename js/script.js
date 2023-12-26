189502a02f3f2c49b6fd0a29739a3ffb870190e2
Serhii Hudzenko <gudzsvmail@gmail.com>
2023-12-26 03:06:57 +0200

    Added scripts to Order form and Subscribe form
    Added scripts to Order form and Subscribe form.
    you can see result in the browser console log
    
    Co-Authored-By: Nelli Diachkina <nelli.diachkina@gmail.com>
    Co-Authored-By: INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
    Co-Authored-By: Tretiak-Anna <151523272+Tretiak-Anna@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -12,3 +12,24 @@ refs.closeModalBtn.addEventListener('click', toogleModal);
 function toogleModal() {
   refs.backdrop.classList.toggle('is-open');
 }
+
+const subscribeForm = document.querySelector('.mail-form');
+const modalForm = document.querySelector('.modal-form');
+
+function formInfo(event) {
+  event.preventDefault();
+  if (event.target.elements.email) {
+    //subscribe
+    console.log('User email:', event.target.elements.email.value);
+    event.target.reset();
+  } else {
+    //modal
+    console.log('User name:', event.target.elements.user_name.value);
+    console.log('User phone:', event.target.elements.user_phone.value);
+    console.log('User email:', event.target.elements.user_email.value);
+    event.target.reset();
+  }
+}
+
+subscribeForm.addEventListener('submit', formInfo);
+modalForm.addEventListener('submit', formInfo);
